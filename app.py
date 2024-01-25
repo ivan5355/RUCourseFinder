021f6d6905aa7cf8871e86e2ae39ac1e9f86c180
ivan <ivanstadnik8@gmail.com>
2024-01-24 23:06:05 -0500

    Got all instructors of a course stored in json

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import json
+from pprint import pprint
+
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__, template_folder='templates')
@@ -12,7 +14,6 @@ def search_page():
 
 @app.route('/search', methods=['POST'])
 def search():
-
     data = request.json
     search_term = data.get('searchTerm')
 
@@ -24,16 +25,32 @@ def search():
     if not matching_courses:
         response_data = {'searchTerm': search_term, 'message': 'No search results found.'}
     else:
-        titles = [course.get('title') for course in matching_courses]
-        instructors = [course.get('instructors', []) for course in matching_courses]
+        results = []
+        for course in matching_courses:
+            course_title = course.get('title')
+            sections = course.get('sections', [])
+
+            instructors_for_course = []
+
+            for section in sections:
+                section_number = section.get('section_number')
+                instructor_for_section = section.get('instructors', [])
+
+                if instructor_for_section not in  instructors_for_course:
+                    instructors_for_course.append(instructor_for_section)
+
+
+            results.append({
+                'instructors': instructors_for_course,
+                'title': course_title,
+
+            })
+
         response_data = {'searchTerm': search_term,
-                         'results': matching_courses,
-                         'titles': titles,
-                         'insturctors' : instructors
+                         'courses': results
                          }
 
-    print("Response Data:", response_data)
-
+    pprint(response_data)
     return jsonify(response_data)
 
 if __name__ == '__main__':
