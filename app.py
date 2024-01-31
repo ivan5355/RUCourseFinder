932410e07f919a828cf7694b6f4123efb08c8856
ivan <ivanstadnik8@gmail.com>
2024-01-31 02:05:16 -0500

    put gpt api call in method and added comments

--- app.py
+++ app.py
@@ -7,26 +7,39 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__, template_folder='templates')
 
-
 with open('rutgers_courses.json', 'r') as json_file:
     courses_data = json.load(json_file)
 
 with open('config.json', 'r') as config_file:
     config = json.load(config_file)
 
+#maps the titles of courses to object
 courses_by_title = {}
 for course in courses_data:
     title = course.get('title').lower()
     courses_by_title[title] = course
 
-
 os.environ["OPENAI_API_KEY"] = config.get("OPENAI_API_KEY")
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-
 
 @app.route('/')
 def search_page():
     return render_template('main.html')
+
+#generates course description by calling chat-gpt api
+def generate_course_description(course_title):
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": f"Tell me about the course {course_title} in 100 words. Make the description as descriptive as possible",
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+
+    return chat_completion.choices[0].message.content
 
 
 @app.route('/search', methods=['POST'])
@@ -35,47 +48,41 @@ def search():
     search_term = data.get('searchTerm')
 
     close_matches = difflib.get_close_matches(search_term.lower(),
-                                              courses_by_title.keys(), n=5) #finds closest matches in coursetitle:courseobject dictionart
+                                              courses_by_title.keys(),
+                                              n=5)
 
     matching_courses = []
 
-    #loops through mactches  and check if the courses by_tit
+    # loops through matches and adds object to matching_course that have a matching title
     for match in close_matches:
         matching_course = courses_by_title.get(match)
-        if matching_course:
-            matching_courses.append(matching_course) #adds object to list that have a matching title
+        matching_courses.append(matching_course)
 
     if not matching_courses:
         return jsonify({'searchTerm': search_term, 'message': 'No search results found.'})
 
     results = []
+
+    #loops through matching_courses to extract the coursecode, coursetitle, and instructors
     for course in matching_courses:
+
         course_string = course.get("courseString")
         course_title = course.get('title')
         sections = course.get('sections', [])
 
         instructors_for_course = []
 
+        #loops through each section to extract the instructors
         for section in sections:
             instructor_for_section = section.get('instructors', [])
 
             if instructor_for_section not in instructors_for_course:
                 instructors_for_course.append(instructor_for_section)
 
-        chat_completion = client.chat.completions.create(
-            messages=[
-                {
-                    "role": "user",
-                    "content": f"Tell me about the course {course_title} in 100 words. Make the description as descriptive as possible",
-                }
-            ],
-            model="gpt-3.5-turbo",
-        )
-
-        chatgptdescription = chat_completion.choices[0].message.content
+        chatgpt_description = generate_course_description(course_title)
 
         results.append({
-            'gptgenerated': chatgptdescription,
+            'gptgenerated': chatgpt_description,
             'instructors': instructors_for_course,
             'title': course_title,
             'course_number': course_string,
